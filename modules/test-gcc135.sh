f160b669e3c81277cc02d6da4bad892afd076a4f
Rene Rivera <grafikrobot@gmail.com>
2019-02-10 03:31:30 +0000

    GCC125 new data plus clang.

--- modules/test-gcc135.sh
+++ modules/test-gcc135.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=/home/grafik/gcc-modules/lib64:/home/grafik/gcc-modules/lib
 export CXX=/home/grafik/gcc-modules/bin/g++-mxx
-OPTS=--dir=/home/grafik/temp/cpp_stats --def-ints --dag-depth=1,151 
+OPTS="--dir=/home/grafik/temp/cpp_stats --def-ints --dag-depth=1,151" 
 python ./parallel_perf.py ${OPTS} --jobs=16 --json-out=data/gcc135-150-j016-gcc.json --toolset=gcc
 python ./parallel_perf.py ${OPTS} --jobs=32 --json-out=data/gcc135-150-j032-gcc.json --toolset=gcc
 python ./parallel_perf.py ${OPTS} --jobs=48 --json-out=data/gcc135-150-j048-gcc.json --toolset=gcc
@@ -10,6 +10,7 @@ python ./parallel_perf.py ${OPTS} --jobs=64 --json-out=data/gcc135-150-j064-gcc.
 python ./parallel_perf.py ${OPTS} --jobs=80 --json-out=data/gcc135-150-j080-gcc.json --toolset=gcc
 python ./parallel_perf.py ${OPTS} --jobs=96 --json-out=data/gcc135-150-j096-gcc.json --toolset=gcc
 python ./parallel_perf.py ${OPTS} --jobs=128 --json-out=data/gcc135-150-j128-gcc.json --toolset=gcc
+export CXX=/home/grafik/clang/bin/clang
 python ./parallel_perf.py ${OPTS} --jobs=16 --json-out=data/gcc135-150-j016-clang.json --toolset=clang
 python ./parallel_perf.py ${OPTS} --jobs=32 --json-out=data/gcc135-150-j032-clang.json --toolset=clang
 python ./parallel_perf.py ${OPTS} --jobs=48 --json-out=data/gcc135-150-j048-clang.json --toolset=clang
