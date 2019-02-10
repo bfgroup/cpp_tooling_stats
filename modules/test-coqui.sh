244768c497423dbcb1ded4329ee9682572112902
Rene Rivera <grafikrobot@gmail.com>
2019-02-10 15:12:55 -0600

    Add Coqui per-compiler stats.

--- modules/test-coqui.sh
+++ modules/test-coqui.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+export CXX=/Developer/Tools/gcc-modules/bin/g++-mxx
 ./parallel_perf.py --dir=${HOME}/devcache/cpp_stats --def-ints --dag-depth=1,151 --jobs=8 --toolset=gcc --json-out=data/coqui-150-j008-gcc.json
+export CXX=/Users/grafik/devcache/clang-7/bin/clang
 ./parallel_perf.py --dir=${HOME}/devcache/cpp_stats --def-ints --dag-depth=1,151 --jobs=8 --toolset=clang --json-out=data/coqui-150-j008-clang.json
